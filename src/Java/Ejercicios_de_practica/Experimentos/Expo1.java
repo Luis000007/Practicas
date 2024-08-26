7ffa2a3446dd86362e6c9202907b219b8bd0806a
Luis000007 <luisemmanuelguzman20@gmail.com>
2024-08-26 03:44:30 -0600

    Vista

--- src/Java/Ejercicios_de_practica/Experimentos/Expo1.java
+++ src/Java/Ejercicios_de_practica/Experimentos/Expo1.java
@@ -1,2 +1,29 @@
-package Java.Ejercicios_de_practica.Experimentos;public class Expo1 {
+package Java.Ejercicios_de_practica.Experimentos;
+
+class Libro {
+    String titulo;
+    String autor;
+    int paginas;
+
+    // Constructor
+    public Libro(String titulo, String autor, int paginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginas = paginas;
+    }
+
+    // Método para mostrar la información del libro
+    public void mostrarInformacion() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Páginas: " + paginas);
+    }
 }
+
+ class RegistroLibro {
+    public static void main(String[] args) {
+        Libro libro1 = new Libro("Cien Años de Soledad", "Gabriel García Márquez", 417);
+        libro1.mostrarInformacion();
+    }
+}
+
