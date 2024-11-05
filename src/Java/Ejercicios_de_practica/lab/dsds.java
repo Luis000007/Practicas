105b4d0ab644473eda20bb130297554ebd2a22bc
Luis000007 <luisemmanuelguzman20@gmail.com>
2024-11-04 18:02:32 -0600

    lab

---
+++ src/Java/Ejercicios_de_practica/lab/dsds.java
@@ -0,0 +1,47 @@
+package Java.Ejercicios_de_practica.lab;
+
+public class dsds {
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+
+            int pivotIndex = partition(arr, low, high);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] <= pivot) {
+                i++;
+
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Arreglo ordenado:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
